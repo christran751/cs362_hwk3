a7452ef69093badf6acc5df44e08125e6290211f
Christopher M Tran <ctran751@g.ucla.edu>
2025-04-30 22:41:45 -0700

    15th commit

--- tests.py
+++ tests.py
@@ -6,7 +6,6 @@ class TestCreditCard(unittest.TestCase):
     """
     Sample docstring
     """
-    random.seed(42)
     valid_length = [15, 16]
     invalid_length = [random.randint(0, 14), 17]
 
@@ -17,7 +16,7 @@ class TestCreditCard(unittest.TestCase):
         return val
 
     def test_credit_card(self):
-        for _ in range(100000):
+        for _ in range(1000000):
             if random.choice([True, False]):
                 length = random.choice(self.invalid_length)
             else:
