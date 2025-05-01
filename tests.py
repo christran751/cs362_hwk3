d4958126d3595112ace8246c6834cfd2df3ce1e7
Christopher M Tran <ctran751@g.ucla.edu>
2025-05-01 00:44:05 -0700

    26th commit

--- tests.py
+++ tests.py
@@ -7,24 +7,17 @@ class TestCreditCard(unittest.TestCase):
     Sample docstring
     """
 
-    length = [16, 17]
 
     def generate_random_number(self, length):
-        val = ""
-        chance = random.choice([1, 2, 3])
-        if chance == 1:
-            length -= 1
-        elif chance == 2:
-            length += 1
-        else:
-            length = length
-
-        for i in range(length):
-            val = val + str(random.randint(0, 9))
+        random_length_offset = random.choice([-1, 0, 1])
+        length += random_length_offset
+        val = ''.join(random.choices("0123456789", k=length))
         return val
 
+
     def test_credit_card(self):
-        for _ in range(1000000):
+        length = [16, 17]
+        for _ in range(100000):
             credit_card_validator(self.generate_random_number(random.choice(self.length)))
 
 
