490e72660dce5260daec6338fa4d0d7e01d1d499
Christopher M Tran <ctran751@g.ucla.edu>
2025-05-01 00:45:05 -0700

    27th commit

--- tests.py
+++ tests.py
@@ -6,17 +6,14 @@ class TestCreditCard(unittest.TestCase):
     """
     Sample docstring
     """
-
-
+    length = [16, 17]
     def generate_random_number(self, length):
         random_length_offset = random.choice([-1, 0, 1])
         length += random_length_offset
         val = ''.join(random.choices("0123456789", k=length))
         return val
 
-
     def test_credit_card(self):
-        length = [16, 17]
         for _ in range(100000):
             credit_card_validator(self.generate_random_number(random.choice(self.length)))
 
