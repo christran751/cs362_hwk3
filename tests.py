be12221d34b556d9205727e6286a0b3e2f10996c
Christopher M Tran <ctran751@g.ucla.edu>
2025-05-01 09:01:37 -0700

    add more testcase

--- tests.py
+++ tests.py
@@ -5,7 +5,8 @@ from credit_card_validator import credit_card_validator
 
 class TestCreditCard(unittest.TestCase):
     """
-    Sample docstring
+    Use random testing to generate random numbers to trigger all 8 bugs in
+    order to come up with theories as to why the bug occured.
     """
     def generate_random_number(self, length):
         random_length_offset = random.choice([-1, 0, 1])
@@ -13,7 +14,7 @@ class TestCreditCard(unittest.TestCase):
         return ''.join(random.choices("0123456789", k=length))
 
     def test_credit_card(self):
-        for _ in range(1000000000): # added two zero remove later
+        for _ in range(694200):
             val = self.generate_random_number(random.randint(15, 16))
             credit_card_validator(val)
 
