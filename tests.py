680fb068952132d7aba35272da8fbaa4c9a6d898
Christopher M Tran <ctran751@g.ucla.edu>
2025-04-30 21:44:54 -0700

    first commit

---
+++ tests.py
@@ -0,0 +1,32 @@
+import random
+import unittest
+from credit_card_validator import credit_card_validator
+
+class TestCreditCard(unittest.TestCase):
+    """
+    Sample docstring
+    """
+    valid_length = [15, 16]
+    invalid_lengths = [12, 13, 14, 17, 18, 19]
+
+    def generate_random_number(self, length):
+        val = ""
+        length = random.choice(invalid_lengths)
+        for i in range(length):
+            val = val + str(random.randint(0, 9))
+        return val
+
+    def test_credit_card(self):
+        for _ in range(10000):
+            if random.choice([True, False]):
+                length = random.choice(self.invalid_lengths)
+            else:
+                length = random.choice(self.invalid_lengths)
+            credit_card_validator(self.generate_random_number(length))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
