94a0d67f9b80a662ca304120b0ae659c9af5f31b
Christopher M Tran <ctran751@g.ucla.edu>
2025-05-01 00:21:21 -0700

    20th commit

--- tests.py
+++ tests.py
@@ -7,8 +7,7 @@ class TestCreditCard(unittest.TestCase):
     Sample docstring
     """
 
-    valid_length = [15, 16]
-    invalid_length = [14, 17]
+    length = random.randint(14, 17)
 
 
     def generate_random_number(self, length):
@@ -18,12 +17,12 @@ class TestCreditCard(unittest.TestCase):
         return val
 
     def test_credit_card(self):
-        for _ in range(1000000):
-            if random.choice([True, False]):
-                length = random.choice(self.invalid_length)
-            else:
-                length = random.choice(self.valid_length)
-            credit_card_validator(self.generate_random_number(length))
+        for _ in range(100000):
+            # if random.choice([True, False]):
+            #     length = random.choice(self.invalid_length)
+            # else:
+            #     length = random.choice(self.valid_length)
+            credit_card_validator(self.generate_random_number(self.length))
 
 
 if __name__ == "__main__":
