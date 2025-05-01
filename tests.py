e06ef3f2df159ef6dd44b46b6f12cf0df6bed27e
Christopher M Tran <ctran751@g.ucla.edu>
2025-04-30 21:56:35 -0700

    sixth commit

--- tests.py
+++ tests.py
@@ -16,9 +16,9 @@ class TestCreditCard(unittest.TestCase):
         return val
 
     def test_credit_card(self):
-        for _ in range(1000000  ):
+        for _ in range(1000000):
             if random.choice([True, False]):
-                length = random.choice(self.invalid_lengths)
+                length = self.invalid_lengths
             else:
                 length = random.choice(self.invalid_lengths)
             credit_card_validator(self.generate_random_number(length))
