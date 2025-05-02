9381afcba122d1ca5c94dcab1dd6af3f5b8e6e5e
Christopher M Tran <ctran751@g.ucla.edu>
2025-05-02 12:42:19 -0700

    reduce testcase

--- tests.py
+++ tests.py
@@ -14,7 +14,7 @@ class TestCreditCard(unittest.TestCase):
         return ''.join(random.choices("0123456789", k=length))
 
     def test_credit_card(self):
-        for _ in range(694200):
+        for _ in range(69420):
             val = self.generate_random_number(random.randint(15, 16))
             credit_card_validator(val)
 
