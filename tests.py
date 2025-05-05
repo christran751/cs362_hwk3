9a7f9cc1da05326c57ba75f226cfb3eaebeb3efb
Christopher M Tran <ctran751@g.ucla.edu>
2025-05-05 14:11:10 -0700

    changed test length 2

--- tests.py
+++ tests.py
@@ -12,6 +12,8 @@ class TestCreditCard(unittest.TestCase):
         """
         Generate a numeric string of a given length
         """
+        random_length_offset = random.choice([-5, -1, 0, 1, 5])
+        length += random_length_offset
         return ''.join(random.choices("0123456789", k=length))
 
     def test_credit_card(self):
@@ -19,7 +21,7 @@ class TestCreditCard(unittest.TestCase):
         Randomized test to explore different cases that might trigger bugs.
         """
         for _ in range(694200):
-            val = self.generate_random_number(random.randint(1, 17))
+            val = self.generate_random_number(random.randint(15, 16))
             credit_card_validator(val)
 
 
